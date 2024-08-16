5a0ece917dff5a17dfa288429b1754c385d70fe0
Muhammad Taufik <muhammad.taufik@adira.co.id>
2024-08-16 15:36:57 +0700

    update code

--- error/resError.js
+++ error/resError.js
@@ -1,12 +1,11 @@
 const moment = require('moment');
-const errCode = require('./errCode');
 const format = require('../config/format');
 
 function resError(code, description, errorDetails = '') {
   return {
     message: 'unsuccessful',
     err_code: code,
-    err_msg: description,
+    err_msg: !format.isEmpty(description) ? description : 'internal server error',
     err_msg2: errorDetails,
     language: 'EN',
     timestamp: format.getCurrentTimeInJakarta(moment().format())
